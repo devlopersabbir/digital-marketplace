d329cc0436e0456aebfbced04e98ef4e9b71e624
devlopersabbir <devlopersabbir@gmail.com>
2023-11-21 16:41:48 +0600

    MaxWithWrapper components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <p>hello</p>;
+  return <MaxWidthWrapper></MaxWidthWrapper>;
 }
---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+type TProps = {
+  className?: string;
+  children: ReactNode;
+};
+
+const MaxWidthWrapper = ({ className, children }: TProps) => {
+  return (
+    <div
+      className={cn(
+        "max-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
