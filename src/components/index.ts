fc4121f0af44bd5e9465fb8b9d75bfd8dd7b1da8
devlopersabbir <devlopersabbir@gmail.com>
2023-11-22 03:03:35 +0600

    dynamic navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,6 @@ export { default as Navbar } from "./navbar/Navbar";
 export { default as NavItem } from "./navbar/NavItem";
 export { default as NavItems } from "./navbar/NavItems";
 
+export { default as Cart } from "./cart/Cart";
+
 export { Icons } from "./Icons";
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import MaxWidthWrapper from "../MaxWidthWrapper";
-import { Icons } from "..";
+import { Icons } from "@/components";
 import NavItems from "./NavItems";
+import { Cart } from "@/components";
+import { buttonVariants } from "../ui/button";
 
 const Navbar = () => {
+  const user = null;
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
@@ -19,6 +22,43 @@ const Navbar = () => {
 
               <div className="hidden lg:ml-8 z-50 lg:block lg:self-stretch">
                 <NavItems />
+              </div>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {!user ? (
+                    <Link
+                      className={buttonVariants({ variant: "ghost" })}
+                      href="sign-in"
+                    >
+                      Sign In
+                    </Link>
+                  ) : null}
+                  {!user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+                  {!user ? (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create Account
+                    </Link>
+                  ) : null}
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
+                  {!user ? (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  ) : null}
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
