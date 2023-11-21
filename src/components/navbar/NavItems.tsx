60add42e8bea286e81e2d422d05257450e6945a2
devlopersabbir <devlopersabbir@gmail.com>
2023-11-22 02:43:24 +0600

    drop down menu close with also keyboard event "Escape"

--- src/components/navbar/NavItems.tsx
+++ src/components/navbar/NavItems.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavItem } from "..";
 import { useOnClickOutside } from "@/hooks";
 
@@ -10,6 +10,18 @@ const NavItems = () => {
   const isAnyOpen = activeIndex !== null;
 
   const navRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
   useOnClickOutside(navRef, () => {
     setActiveIndex(null);
   });
